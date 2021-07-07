418dd70ac29a7b22cc75d0dc4822e72689713434
sjshetti <sanket151001@gmail.com>
2021-07-07 21:27:01 +0530

    Added fixed point Simulink Model for tracking

--- MATLAB/Tracking/trackingSimulink/config.m
+++ MATLAB/Tracking/trackingSimulink/config.m
@@ -1,5 +1,9 @@
 %Run this before simulating trackingModel.slx
-% fc = 4128460;     %Carrier Frequency detected after acquisition
+fc = 4128460;     %Carrier Frequency detected after acquisition
 fs = 16367600;    %Sampling frequency
-fcBasis = 4128460;
+fcBasis = fc;
 svnum = 22;       %Satellite Number
+
+%For carrier phase error lookup table
+QIratio = -5:(2^-7):5;            %Covers -78.7 to 78.7 degrees. 1281 elements
+carrErrorValues = atan(QIratio)/(2.0*pi);
