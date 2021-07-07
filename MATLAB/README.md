26919ed0d060d3e3b8448d045a336358b60b7faa
nishantmi <63803517+nishantmi@users.noreply.github.com>
2021-07-07 17:20:27 +0530

    Integrated Acquisition and Tracking modules

---
+++ MATLAB/README.md
@@ -0,0 +1,2 @@
+# SURP_NAVIC
+This repo contains files pertaining to NAVIC development under Prof. Rajesh Zele from IITB during the SURP of Summer '21
--- MATLAB/Tracking/trackingSimulink/config.m
+++ MATLAB/Tracking/trackingSimulink/config.m
@@ -1,5 +1,5 @@
 %Run this before simulating trackingModel.slx
-fc = 4128460;     %Carrier Frequency detected after acquisition
+% fc = 4128460;     %Carrier Frequency detected after acquisition
 fs = 16367600;    %Sampling frequency
-fcBasis = fc;
+fcBasis = 4128460;
 svnum = 22;       %Satellite Number
